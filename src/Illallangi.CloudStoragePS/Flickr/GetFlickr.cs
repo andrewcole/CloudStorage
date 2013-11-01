bb7ff005beed7204cb26a0f9bf68ef08bb1cafb8
Andrew Cole <andrew.cole@illallangi.com>
2013-11-01 12:51:27 +1100

    Added GetFlickr

---
+++ src/Illallangi.CloudStoragePS/Flickr/GetFlickr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation;
+using DropNet;
+using DropNet.Exceptions;
+using Illallangi.CloudStoragePS.Config;
+
+namespace Illallangi.CloudStoragePS.Flickr
+{
+    [Cmdlet(VerbsCommon.Get, "DropBoxAccountInfo")]
+    public sealed class GetDropBoxAccountInfo : FlickrPSCmdlet
+    {
+        protected override IEnumerable<object> Process(DropNetClient client)
+        {
+            var accountInfo = client.AccountInfo();
+
+            yield return new
+            {
+                DisplayName = accountInfo.display_name,
+                EMail = accountInfo.email,
+                UserID = accountInfo.uid,
+                Country = accountInfo.country,
+                ReferralLink = accountInfo.referral_link,
+            };
+        }
+    }
+}
