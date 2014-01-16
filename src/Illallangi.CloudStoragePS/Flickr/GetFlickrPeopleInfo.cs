79b3c85eba1958586348f1130a4de2ca423f6cab
Andrew Cole <andrew.cole@illallangi.com>
2014-01-16 23:00:30 +1100

    Added Get-FlickrPeopleInfo

---
+++ src/Illallangi.CloudStoragePS/Flickr/GetFlickrPeopleInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Illallangi.CloudStoragePS.Flickr
+{
+    [Cmdlet(VerbsCommon.Get, "FlickrPeopleInfo")]
+    public sealed class GetFlickrPeopleInfo : FlickrPSCmdlet
+    {
+        protected override IEnumerable<object> Process(FlickrNet.Flickr client)
+        {
+            yield return client.PeopleGetInfo(client.TestLogin().UserId);
+        }
+    }
+}
