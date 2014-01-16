dcb66b1574daa3c5046b55be15126921bde00e2b
Andrew Cole <andrew.cole@illallangi.com>
2014-01-17 00:53:22 +1100

    Added Get-FlickrPhotoset cmdlet

---
+++ src/Illallangi.CloudStoragePS/Flickr/GetFlickrPhotoset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+
+using FlickrNet;
+
+namespace Illallangi.CloudStoragePS.Flickr
+{
+    [Cmdlet(VerbsCommon.Get, "FlickrPhotoset")]
+    public sealed class GetFlickrPhotoset : FlickrPSCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        public string UserId { get; set; }
+
+        protected override IEnumerable<object> Process()
+        {
+            PhotosetCollection collection = null;
+            do
+            {
+                collection = this.Client.PhotosetsGetList(this.UserId, null == collection ? 0 : collection.Page + 1, 10);
+                foreach (var set in collection)
+                {
+                    yield return set;
+                }
+            }
+            while (collection.Page < collection.Pages);
+        }
+    }
+}
