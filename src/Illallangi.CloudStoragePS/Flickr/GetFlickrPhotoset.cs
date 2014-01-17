9fc3cbe75b55deea2404ef7d1c491696db92c8f8
Andrew Cole <andrew.cole@illallangi.com>
2014-01-17 14:00:21 +1100

    Added PhotoId option

--- src/Illallangi.CloudStoragePS/Flickr/GetFlickrPhotoset.cs
+++ src/Illallangi.CloudStoragePS/Flickr/GetFlickrPhotoset.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Management.Automation;
 
 using FlickrNet;
@@ -8,21 +9,55 @@ namespace Illallangi.CloudStoragePS.Flickr
     [Cmdlet(VerbsCommon.Get, "FlickrPhotoset")]
     public sealed class GetFlickrPhotoset : FlickrPSCmdlet
     {
-        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true)]
+        #region Fields
+
+        private const string PhotosetsGetList = "PhotosetsGetList";
+
+        private const string PhotosGetAllContexts = "PhotosGetAllContexts";
+
+        #endregion
+
+        #region Properties
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = GetFlickrPhotoset.PhotosetsGetList)]
         public string UserId { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = GetFlickrPhotoset.PhotosGetAllContexts)]
+        public string PhotoId { get; set; }
+
+        #endregion
+
+        #region Methods
 
         protected override IEnumerable<object> Process()
         {
-            PhotosetCollection collection = null;
-            do
+            switch (this.ParameterSetName)
             {
-                collection = this.Client.PhotosetsGetList(this.UserId, null == collection ? 0 : collection.Page + 1, 10);
-                foreach (var set in collection)
-                {
-                    yield return set;
-                }
+                case GetFlickrPhotoset.PhotosGetAllContexts:
+                    foreach (var set in this.Client.PhotosGetAllContexts(this.PhotoId).Sets)
+                    {
+                        yield return this.Client.PhotosetsGetInfo(set.PhotosetId);
+                    }
+
+                    break;
+                case GetFlickrPhotoset.PhotosetsGetList:
+                    PhotosetCollection collection = null;
+                    do
+                    {
+                        collection = this.Client.PhotosetsGetList(this.UserId, null == collection ? 0 : collection.Page + 1, 10);
+                        foreach (var set in collection)
+                        {
+                            yield return set;
+                        }
+                    }
+                    while (collection.Page < collection.Pages);
+
+                    break;
+                default:
+                    throw new NotImplementedException(this.ParameterSetName);
             }
-            while (collection.Page < collection.Pages);
         }
+
+        #endregion
     }
 }
