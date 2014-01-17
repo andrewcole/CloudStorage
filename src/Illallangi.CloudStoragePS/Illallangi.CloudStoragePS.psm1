6f43487fbd799657420c7da89553134cb1a9a5d9
Andrew Cole <andrew.cole@illallangi.com>
2014-01-17 14:21:56 +1100

    Added BackupFlickr

--- src/Illallangi.CloudStoragePS/Illallangi.CloudStoragePS.psm1
+++ src/Illallangi.CloudStoragePS/Illallangi.CloudStoragePS.psm1
@@ -55,3 +55,25 @@ Function Push-ChildItem
 		}
 	}
 }
+
+Function Backup-Flickr
+{
+	param(
+		[string]$UserName = (Get-FlickrPeopleInfo).UserName
+	)
+	Process
+	{
+		$user = Get-FlickrPeopleInfo -UserName "$($UserName)"
+		foreach ($photoSet in Get-FlickrPhotoSet -UserId $user.UserId)
+		{
+			foreach ($photo in Get-FlickrPhoto -PhotoSetId $photoSet.PhotoSetId)
+			{
+				$set = (Get-FlickrPhotoset -PhotoId $photo.PhotoId | Sort-Object { $_.NumberOfPhotos } | Select-Object -Last 1)
+				Write-Host "$($set.Title)\$($photo.Title) ($($photo.PhotoId)).$($photo.OriginalFormat.ToLower())"
+				New-Item -Type Directory -Path "$($user.UserName)\$($set.Title)" -ErrorAction SilentlyContinue
+				$photo.Description | Out-File -FilePath "$($user.UserName)\$($set.Title)\$($photo.Title) ($($photo.PhotoId)).html"
+				Backup-FlickrPhoto -Photo $photo -Path "$($user.UserName)\$($set.Title)\$($photo.Title) ($($photo.PhotoId)).$($photo.OriginalFormat)" -Type Small
+			}
+		}
+	}
+}
